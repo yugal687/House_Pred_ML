ecbbdb787dece4fe3d835836f7ee23af1f9ff1fa
yugal687 <pageniyugal@gmail.com>
2025-03-26 21:53:31 -0500

    added slash

--- house-price-frontend/src/components/PredictionForm.js
+++ house-price-frontend/src/components/PredictionForm.js
@@ -33,7 +33,7 @@ export default function PredictionForm() {
       //   ),
       // });
   
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
